3bcd6ec333408b4cef98c50c951a646f26bbf9fa
Paul Coroneos <paul@pcoroneos.com>
2021-06-29 20:34:12 -0600

    add/remove functionality

--- components/Todolist.tsx
+++ components/Todolist.tsx
@@ -1,7 +1,11 @@
-export const Todolist = () => {
-  const tempItems = ['buy bananas', 'finish todolist']
+import { useState } from 'react'
 
-  return <ul className="m-4 p-4 border rounded-md border-gray-500 divide-y">{tempItems.map((item, idx) =>
-    <li className="py-4" key={`${item}${idx}`}><input type="checkbox" />{' '}{item}</li>
-  )}</ul>
+export const Todolist = () => {
+  const [todos, setTodos] = useState(['buy bananas', 'finish todolist'])
+
+  return <div className="m-2"><ul className="p-4 border rounded-md border-gray-500 divide-y">{todos.map((item, idx) =>
+    <li className="py-4"><label><input onClick={() => {
+      todos.splice(idx, 1);
+      setTodos([...todos])} }type="checkbox" />{' '}{item}</label></li>
+  )}</ul><div className="flex justify-end"><button onClick={() => setTodos([...todos, ' '])} className="btn btn-primary mt-2">Add item</button></div></div>
 }
